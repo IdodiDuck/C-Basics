a2d92711b1e002e0464259658b6d728b1697e197
idoredid123 <ido.redid@gmail.com>
2023-01-07 11:08:24 +0200

    Update

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    int num1, num2;
+
+    printf("Enter number 1:");
+    scanf("%d", &num1);
+
+    printf("Enter number 2: ");
+    scanf("%d", &num2);
+
+    int sum;
+    sum = num1 + num2;
+
+    printf("%d + %d = %d", num1, num2, sum);
+
     return 0;
 }

00f9c36d9cda5168915d38c3f1ec438a3d55fb6f
Ido Redid <Ido.redid@gmail.com>
2023-01-21 20:54:21 +0200

    update

--- main.c
+++ main.c
@@ -2,17 +2,15 @@
 
 int main() {
     int num1, num2;
+    /* My first C Program :D */
 
-    printf("Enter number 1:");
+    printf("Enter number 1: ");
     scanf("%d", &num1);
 
     printf("Enter number 2: ");
     scanf("%d", &num2);
 
-    int sum;
-    sum = num1 + num2;
+    printf("%d + %d = %d", num1, num2, num1 + num2);
 
-    printf("%d + %d = %d", num1, num2, sum);
-
-    return 0;
+    
 }
